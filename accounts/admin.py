28d31984613e56cbcddc2339cc1033220f7fe689
JH <johno.hernandezm@gmail.com>
2019-02-21 19:21:03 -0500

    Extended user model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,7 @@
 # from django.contrib import admin
 # from models import accounts 
+from django.contrib import admin
 
+from accounts.models import UserProfile
 # Register your models here.
+admin.site.register(UserProfile)
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+
+class UserProfile(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	cellphone = models.IntegerField(default=0)
